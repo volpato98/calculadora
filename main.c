8766a305c41c256f1ea9036e010cbd3816fc767a
volpato98 <ra98528@uem.br>
2018-09-12 16:54:06 -0300

    ignore

--- main.c
+++ main.c
@@ -17,12 +17,9 @@ int main(){
 	int x,y,op;
 	printf("digite um numero para x e outro para y: ");
 	scanf("%d %d",&x,&y);	
-	printf("\nDigite uma operação:\n
-		1 - soma\n
-		2 - subtraçao\n
-		3 - multiplicacao\n
-		4 - divisao\n");
+	printf("\nDigite uma operação:\n1 - soma\n2 - subtraçao\n3 - multiplicacao\n4 - divisao\n");
 	scanf ("%i",&op);
+	switch(op) 
 	return 0;	
 			
 }
